7676da776c8e851ad56a39d282f4e00cf9dfea55
Neet2work <germannlars977@gmail.com>
2024-11-28 11:16:38 +0100

    dfg

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Viewport" content="width=device-width, initial-scale=1.0">
+        <title>meinewebsite</title>
+    </head>
+    <body>
+        <header>
+            <div style="height: 200px"></div>
+        </header>
+
+        <div></div>
+    </body>
+</html>
